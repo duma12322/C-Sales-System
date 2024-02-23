8f5d042555aec73f4201dd95db3be282bcc33a57
duma12322 <anais_asdm@hotmail.com>
2024-02-23 18:31:25 -0400

    C# Ventas

---
+++ CapaDatos/Conexion.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CapaDatos
+{
+    public class Conexion
+    {
+        public static string CN = "Server=MARCE\\SQLEXPRESS; DataBase=DBPRUEBAS; Trusted_Connection=True; TrustServerCertificate=True";
+    }
+}
